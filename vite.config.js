73d85ba2a15f953446e6544a43cdec13511fabd6
Dabi <uchennabreaktru@gmail.com>
2025-11-25 17:15:13 +0100

    Setup for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/cgpa-calculator-udevs/",
+  base: "/cgpa-calculator/",
 });
 defineConfig;
